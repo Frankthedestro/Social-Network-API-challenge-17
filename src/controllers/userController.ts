212797f3bd8ea13e4f7bfc88769ebfe4f36a8d5f
Hunter <hunterwest94@rocketmail.com>
2025-04-17 20:29:04 -0400

    updating naming/ finishing user controller and route requirement

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,8 @@
 import User from '../models/User.js';
 import { Request, Response } from 'express';
 
+
+  // get all users
   export const getUsers = async (_req: Request, res: Response) => {
     try {
       const users = await User.find();
@@ -10,6 +12,7 @@ import { Request, Response } from 'express';
     }
   }
 
+  // get a single user by id
   export const getSingleUser = async (req: Request, res: Response) => {
     try {
       const user = await User.findOne({ _id: req.params.userId })
@@ -17,7 +20,7 @@ import { Request, Response } from 'express';
         .populate('posts');
 
       if (!user) {
-        res.status(404).json({ message: 'No user with that ID' });
+        res.status(404).json({ message: 'Try again Homie they dont exist.' });
       } else {
         res.json(user);
       }
@@ -29,10 +32,78 @@ import { Request, Response } from 'express';
   // create a new user
   export const createUser = async (req: Request, res: Response) => {
     try {
-      const dbUserData = await User.create(req.body);
-      res.json(dbUserData);
+      const user = await User.create(req.body);
+      res.json(user);
     } catch (err) {
       res.status(500).json(err);
     }
   }
 
+  // update a user by id
+  export const updateUser = async (req: Request, res: Response) => { 
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { new: true, runValidators: true }
+      );
+
+      if (!user) {
+        res.status(404).json({ message: 'This user does not exist. Just like the limit!' });
+        return;
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+  // delete a user by id
+  export const deleteUser = async (req: Request, res: Response) => {  
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        res.status(404).json({ message: 'They are already gone homie...' });
+        return;
+      }
+
+      res.json({ message: 'User got got!' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+  // add a friend
+  export const addFriend = async (req: Request, res: Response) => {
+    try {
+        const user = await User.findOneAndUpdate(
+          { _id: req.params.userId },
+          { $addToSet: { friends: req.params.friendId } },
+          { new: true }
+        );
+        if (!user) {
+          res.status(404).json({ message: 'They dont exist... you sure you are friends?' });
+          return;
+        }
+        res.json(user);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+  }
+  // remove a friend 
+  export const removeFriend = async (req: Request, res: Response) => {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId } },
+        { new: true }
+      );
+      if (!user) {
+        res.status(404).json({ message: 'Friendship ended!' });
+        return;
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
--- src/routes/api/userRoutes.ts
+++ src/routes/api/userRoutes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 const router = Router();
-import { getUsers, getSingleUser, createUser } from '../../controllers/userController.js';
+import { getUsers, getSingleUser, createUser, updateUser, deleteUser, addFriend, removeFriend } from '../../controllers/userController.js';
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
 router.route('/:userId').get(getSingleUser);
+router.route('/:userId').put(updateUser);
+router.route('/:userId').delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 export default router;
